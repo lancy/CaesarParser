867d514ee9000957ac0b249e543fc5e30982f514
lancy <lancy1014@gmail.com>
2016-01-06 21:45:53 +0800

    ADD: Tests

---
+++ CaesarParserTests/JSONDeserializationTests.swift
@@ -0,0 +1,267 @@
+//
+//  CaesarParserTests.swift
+//  CaesarParserTests
+//
+//  Created by lancy on 5/17/15.
+//  Copyright (c) 2015 Chenyu Lan. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import CaesarParser
+
+class JSONDeserializationTests: XCTestCase {
+
+    func JSONStringToJSONObject(jsonString: String) -> JSONObject? {
+        let data: NSData = jsonString.dataUsingEncoding(NSUTF8StringEncoding)!
+        return try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0))
+    }
+
+    let dummyResponse = [
+        "string": "a",
+        "int": 1,
+        "int_string": "1",
+        "bool": true,
+        "date": "2014-09-19",
+        "url": "http://github.com/",
+        "stringArray": ["a", "b", "c"],
+        "intArray": [1, 2, 3, 4, 5],
+        "boolArray": [true, false],
+        "instance": [
+            "name": "b"
+        ],
+        "instanceArray": [
+            [
+                "name": "c"
+            ], [
+                "name": "d"
+            ]
+        ],
+        "instanceMap": [
+            "ePerson": [
+                "name": "e"
+            ],
+            "fPerson": [
+                "name": "f"
+            ]
+        ]
+    ]
+
+    struct Person: Deserializable {
+        var name = ""
+
+        init(json: JSONDictionary) {
+            name <-- json["name"]
+        }
+
+        init() {}
+    }
+
+    enum EnumTest: Int {
+        case Zero = 0
+        case One = 1
+    }
+
+    func testOptionalString() {
+        var property: String?
+        property <-- dummyResponse["string"]
+        XCTAssertEqual(property!, "a", "String? property should equal 'a'")
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertNil(property, "String? property should equal nil after invalid assignment")
+    }
+
+    func testString() {
+        var property = "b"
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertEqual(property, "b", "String property should have the default value 'b'")
+        property <-- dummyResponse["string"]
+        XCTAssertEqual(property, "a", "String property should equal 'a'")
+    }
+
+    func testOptionalInt() {
+        var property: Int?
+        property <-- dummyResponse["int"]
+        XCTAssertEqual(property!, 1, "Int? property should equal 1")
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertNil(property, "Int? property should equal nil after invalid assignment")
+    }
+
+    func testInt() {
+        var property = 2
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertEqual(property, 2, "Int property should have the default value 2")
+        property <-- dummyResponse["int"]
+        XCTAssertEqual(property, 1, "Int property should equal 1")
+    }
+
+    func testStringToOptionalInt() {
+        var number: Int?
+        number <-- dummyResponse["int_string"]
+        XCTAssertEqual(number!, 1, "Strings containing numbers should successfully deserialize into optional Ints.")
+    }
+
+    func testStringToInt() {
+        var number = 0
+        number <-- dummyResponse["int_string"]
+        XCTAssertEqual(number, 1, "Strings containing numbers should successfully deserialize into Ints.")
+    }
+
+    func testOptionalBool() {
+        var property: Bool?
+        property <-- dummyResponse["bool"]
+        XCTAssertEqual(property!, true, "Bool? property should equal true")
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertNil(property, "Bool? property should equal nil after invalid assignment")
+    }
+
+    func testBool() {
+        var property = true
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertEqual(property, true, "Bool property should have the default value true")
+        property <-- dummyResponse["bool"]
+        XCTAssertEqual(property, true, "Bool property should equal true")
+    }
+
+    func testOptionalNSDate() {
+        var property: NSDate?
+        property <-- (dummyResponse["date"], DateFormatConverter("yyyy-MM-dd"))
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let testDate = dateFormatter.dateFromString("2014-09-19")
+        XCTAssertEqual(property!.compare(testDate!), NSComparisonResult.OrderedSame, "NSDate? property should equal 2014-09-19")
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertNil(property, "NSDate? property should equal nil after invalid assignment")
+    }
+
+    func testNSDate() {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let defaultTestDate = dateFormatter.dateFromString("2015-09-19")
+        var property = defaultTestDate!
+        property <-- (dummyResponse["invalidKey"], DateFormatConverter("yyyy-MM-dd"))
+        XCTAssertEqual(property.compare(defaultTestDate!), NSComparisonResult.OrderedSame, "NSDate should have the default value 2015-09-19")
+        property <-- (dummyResponse["date"], DateFormatConverter("yyyy-MM-dd"))
+        let testDate = dateFormatter.dateFromString("2014-09-19")
+        XCTAssertEqual(property.compare(testDate!), NSComparisonResult.OrderedSame, "NSDate should have the value 2015-09-19")
+    }
+
+    func testOptionalNSURL() {
+        var property: NSURL?
+        property <-- dummyResponse["url"]
+        XCTAssertEqual(property!.host!, "github.com", "NSURL? property should equal github.com")
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertNil(property, "NSURL? property should equal nil after invalid assignment")
+    }
+
+    func testNSURL() {
+        var property = NSURL(string: "http://google.com")!
+        property <-- dummyResponse["invalidKey"]
+        XCTAssertEqual(property.host!, "google.com", "NSURL should have the default value google.com")
+        property <-- dummyResponse["url"]
+        XCTAssertEqual(property.host!, "github.com", "NSURL should have the value github.com")
+    }
+
+    func testStringArray() {
+        var property = [String]()
+        property <-- dummyResponse["stringArray"]
+        XCTAssertEqual(property.count, 3, "[String] property should have 3 members")
+    }
+
+    func testIntArray() {
+        var property = [Int]()
+        property <-- dummyResponse["intArray"]
+        XCTAssertEqual(property.count, 5, "[Int] property should have 5 members")
+    }
+
+    func testBoolArray() {
+        var property = [Bool]()
+        property <-- dummyResponse["boolArray"]
+        XCTAssertEqual(property.count, 2, "[Bool] property should have 2 members")
+    }
+
+    func testInstance() {
+        var instance = Person()
+        instance <-- dummyResponse["instance"]
+        XCTAssertEqual(instance.name, "b", "Person instance's name property should equal 'b'")
+    }
+
+    func testInstanceArray() {
+        var property = [Person]()
+        property <-- dummyResponse["instanceArray"]
+        XCTAssertEqual(property.count, 2, "[Person] property should have 2 members")
+    }
+
+    func testInstanceMap() {
+        var property = [String:Person]()
+        property <-- dummyResponse["instanceMap"]
+        XCTAssertEqual(property["ePerson"]!.name, "e", "member \"ePerson\" of [String:Person] property should have \"e\" for name")
+    }
+
+    func testRawValueEnum() {
+        var property = EnumTest.Zero
+        property <-- dummyResponse["int"]
+        XCTAssertEqual(property, EnumTest.One, "EnumTest should be equal to .One")
+    }
+
+    func testRawValueEnumArray() {
+        var property: [EnumTest]?
+        property <-- dummyResponse["intArray"]
+        XCTAssertEqual(property?.count ?? 0, 1, "[EnumTest] property should have 1 member")
+    }
+
+    func testJSONStringArrayParsing() {
+        let jsonString = "[{\"name\": \"I am \"},{\"name\": \"Groot!\"}]"
+        let jsonObject: JSONObject? = JSONStringToJSONObject(jsonString)
+        var people = [Person]()
+        var areYouGroot = ""
+
+        people <-- jsonObject
+
+        for person in people {
+            areYouGroot += person.name
+        }
+
+        XCTAssertEqual(areYouGroot, "I am Groot!", "Groot should be Groot")
+    }
+
+    func testJSONStringMapParsing() {
+        let jsonString = "{\"person one\": {\"name\": \"I am \"}, \"person two\": {\"name\": \"Groot!\"}}"
+        let jsonObject: JSONObject? = JSONStringToJSONObject(jsonString)
+        var people = [String:Person]()
+        var areYouGroot = ""
+        var keys = ""
+
+        people <-- jsonObject
+
+        for (personKey, person) in people {
+            areYouGroot += person.name
+            keys += personKey
+        }
+
+        XCTAssertEqual(keys, "person oneperson two", "keys should be correctly picked up")
+        XCTAssertEqual(areYouGroot, "I am Groot!", "Groot should be Groot")
+    }
+    
+    func testJSONIntObjectMap() {
+        let jsonString = "{\"1\": {\"name\": \"Grace\"}, \"2\": {\"name\": \"Lancy\"}}"
+        let jsonObject: JSONObject? = JSONStringToJSONObject(jsonString)
+        var people = [Int: Person]()
+        
+        people <-- jsonObject
+        
+        XCTAssertEqual(people[1]!.name, "Grace", "people[1] should be Grace")
+        XCTAssertEqual(people[2]!.name, "Lancy", "people[2] should be Lancy")
+    }
+    
+    func testJSONIntIntMap() {
+        let jsonString = "{\"1\": 1, \"2\": 2}"
+        let jsonObject: JSONObject? = JSONStringToJSONObject(jsonString)
+        var dict = [Int: Int]()
+        
+        dict <-- jsonObject
+        
+        XCTAssertEqual(dict[1]!, 1, "dict[1] should be 1")
+        XCTAssertEqual(dict[2]!, 2, "dict[2] should be 2")
+    }
+    
+}
---
+++ CaesarParserTests/JSONSerializationTests.swift
@@ -0,0 +1,236 @@
+//
+//  JSONSerializationTests.swift
+//  Caesar
+//
+//  Created by lancy on 5/17/15.
+//  Copyright (c) 2015 Chenyu Lan. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import CaesarParser
+
+
+class JSONSerializationTests: XCTestCase {
+    enum Sex: Int {
+        case Unknown = 0
+        case Male = 1
+        case Female = 2
+    }
+
+    class Person: Serializable {
+        var name = ""
+        var age = 0
+        var birthday: Double = 0.0
+        var weight: Float = 0.0
+        var adult = false
+        var sex = Sex.Unknown
+        var girlFriend: Person?
+        var friends = [Person]()
+        var luckyNumbers = [Int]()
+        var favouredSingers = [String: Person]()
+        var vips = [Int: Person]()
+        var preferNumbers = [Int: Int]()
+        var orientation = [Sex]()
+
+        func toJSONObject() -> JSONObject {
+            var json = JSONDictionary()
+
+            name --> json["name"]
+            age --> json["age"]
+            birthday --> json["birthday"]
+            weight --> json["weight"]
+            adult --> json["adult"]
+            sex --> json["sex"]
+            girlFriend --> json["girlFriend"]
+            friends --> json["friends"]
+            luckyNumbers --> json["luckyNumbers"]
+            favouredSingers --> json["favouredSingers"]
+            vips --> json["vips"]
+            preferNumbers --> json["preferNumbers"]
+            orientation --> json["orientation"]
+
+            return json
+        }
+
+        init() {}
+
+        init(name: String) {
+            self.name = name
+        }
+    }
+
+    func testPrimitiveType() {
+        let person = Person(name: "lancy")
+        person.age = 23
+        person.birthday = 14
+        person.weight = 60
+        person.adult = true
+
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "lancy", "my name is lancy")
+            XCTAssertEqual(dict["age"] as! Int, 23, "I'm 23")
+            XCTAssertEqual(dict["birthday"] as! Double, 14, "my birthday is 14")
+            XCTAssertEqual(dict["weight"] as! Float, 60, "weight 60!")
+            XCTAssertEqual(dict["adult"] as! Bool, true, "i'm an adult")
+        } else {
+            XCTFail("JSON Object should be a dictionary")
+        }
+    }
+
+    func testEnumType() {
+        let person = Person(name: "God")
+        person.sex = .Female
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["sex"] as! Int, Sex.Female.rawValue, "God is a girl")
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+    }
+
+    func testEnumTypeArray() {
+        let person = Person(name: "siri")
+        person.orientation = [.Male, .Female, .Unknown]
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "siri", "my name is siri")
+            if let orientation = dict["orientation"] as? [Int] {
+                XCTAssertEqual(orientation[0] as! Int, Sex.Male.rawValue, "siri likes male!")
+                XCTAssertEqual(orientation[1] as! Int, Sex.Female.rawValue, "siri also likes female!")
+                XCTAssertEqual(orientation[2] as! Int, Sex.Unknown.rawValue, "and siri likes all others!")
+            } else {
+                XCTFail("JSON Object of orientation should be a [Int]")
+            }
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+    }
+
+    func testNestedType() {
+        let person = Person(name: "lancy")
+        let girl = Person(name: "grace")
+        person.girlFriend = girl
+
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "lancy", "my name is lancy")
+            if let girlDict = dict["girlFriend"] as? JSONDictionary {
+                XCTAssertEqual(girlDict["name"] as! String, "grace", "my girl is grace")
+            } else {
+                XCTFail("JSON Object of girl friend should be a dictionary")
+            }
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+    }
+
+    func testObjectArray() {
+        let person = Person(name: "lancy")
+        let friend1 = Person(name: "grace")
+        let friend2 = Person(name: "cambi")
+        person.friends = [friend1, friend2]
+
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "lancy", "my name is lancy")
+            if let friends = dict["friends"] as? [JSONDictionary] {
+                XCTAssertEqual(friends[0]["name"] as! String, "grace", "grace is my friend")
+                XCTAssertEqual(friends[1]["name"] as! String, "cambi", "cambi is my friend")
+            } else {
+                XCTFail("JSON Object of friends friend should be a [JSONDictionary]")
+            }
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+    }
+
+    func testIntArray() {
+        let person = Person(name: "lancy")
+        person.luckyNumbers = [8, 14, 55]
+
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "lancy", "my name is lancy")
+            if let friends = dict["luckyNumbers"] as? [Int] {
+                XCTAssertEqual(friends[0], 8, "lucky 8!")
+                XCTAssertEqual(friends[1], 14, "lucky 14!")
+                XCTAssertEqual(friends[2], 55, "lucky 55! Give me!")
+            } else {
+                XCTFail("JSON Object of friends friend should be a [Int]")
+            }
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+    }
+
+    func testStringObjectMap() {
+        let person = Person(name: "lancy")
+        person.favouredSingers = [
+            "first": Person(name: "sia"),
+            "second": Person(name: "lana")
+        ]
+
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "lancy", "my name is lancy")
+            if let singers = dict["favouredSingers"] as? JSONDictionary {
+                XCTAssertEqual((singers["first"] as! JSONDictionary)["name"] as! String, "sia", "sia the best!")
+                XCTAssertEqual((singers["second"] as! JSONDictionary)["name"] as! String, "lana", "lana go lana!")
+            } else {
+                XCTFail("JSON Object of friends friend should be a JSON dictionary")
+            }
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+    }
+
+    func testIntObjectMap() {
+        let person = Person(name: "lancy")
+        person.vips = [
+            0: Person(name: "lover and family"),
+            1: Person(name: "best friends"),
+            2: Person(name: "maybe boss")
+        ]
+
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "lancy", "my name is lancy")
+            if let persons = dict["vips"] as? JSONDictionary {
+                XCTAssertEqual((persons["0"] as! JSONDictionary)["name"] as! String, "lover and family", "My love My Life! Very Importance!")
+                XCTAssertEqual((persons["1"] as! JSONDictionary)["name"] as! String, "best friends", "BFF!")
+                XCTAssertEqual((persons["2"] as! JSONDictionary)["name"] as! String, "maybe boss", "Please raise the pay...")
+
+            } else {
+                XCTFail("JSON Object of friends friend should be a JSON dictionary")
+            }
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+    }
+
+    func testIntIntMap() {
+        let person = Person(name: "lancy")
+        person.preferNumbers = [
+            0: 1,
+            1: 2,
+            2: 4,
+            3: 8,
+            4: 16,
+            5: 32,
+            6: 64
+        ]
+
+        if let dict = person.toJSONObject() as? JSONDictionary {
+            XCTAssertEqual(dict["name"] as! String, "lancy", "my name is lancy")
+            if let singers = dict["preferNumbers"] as? JSONDictionary {
+                XCTAssertEqual(singers["0"] as! Int, 1, "1")
+                XCTAssertEqual(singers["1"] as! Int, 2, "2")
+                XCTAssertEqual(singers["2"] as! Int, 4, "4")
+                XCTAssertEqual(singers["3"] as! Int, 8, "8")
+                XCTAssertEqual(singers["4"] as! Int, 16, "16")
+                XCTAssertEqual(singers["5"] as! Int, 32, "32")
+                XCTAssertEqual(singers["6"] as! Int, 64, "64")
+            } else {
+                XCTFail("JSON Object of friends friend should be a [JSONDictionary]")
+            }
+        } else {
+            XCTFail("JSON Object of Person should be a dictionary")
+        }
+
+    }
+
+}
